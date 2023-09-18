05120ac03950bc6983211ab3845e2caad27bd997
JuMaxi <jmaximovski@gmail.com>
2023-09-18 19:58:48 +0100

    SolutionDay06_Part02

--- AdventOfCode2022_Day05/UnitTest1.cs
+++ AdventOfCode2022_Day05/UnitTest1.cs
@@ -36,6 +36,40 @@ namespace AdventOfCode2022_Day06
 
             return 0;
         }
+
+        public int Part02(string input)
+        {
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+
+                int next = 1;
+
+                bool equal = false;
+
+                while (next <= 13)
+                {
+                    for (int ii = i + next; ii <= i + 13; ii++)
+                    {
+                        if (c == input[ii])
+                        {
+                            equal = true;
+                            break;
+                        }
+                    }
+
+                    c = input[i + next];
+                    next++;
+                }
+                if (equal == false)
+                {
+                    return i + 14;
+                }
+            }
+
+            return 0;
+        }
     }
 
     public class Day06
@@ -56,6 +90,27 @@ namespace AdventOfCode2022_Day06
 
             string text = File.ReadAllText("C:\\Dev\\Advent-Of-Code-2022\\AdventOfCode2022_Day05\\input_day06.txt");
             int result = solution.Part01(text);
+        }
+
+        [Fact]
+        public void Day06_Part2()
+        {
+            SolutionDay06 solution = new();
+            int resultA = solution.Part02("bvwbjplbgvbhsrlpgdmjqwftvncz");
+            int resultB = solution.Part02("nppdvjthqldpwncqszvftbrmjlhg");
+            int resultC = solution.Part02("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg");
+            int resultD = solution.Part02("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw");
+            int resultE = solution.Part02("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+
+            Assert.Equal(23, resultA);
+            Assert.Equal(23, resultB);
+            Assert.Equal(29, resultC);
+            Assert.Equal(26, resultD);
+            Assert.Equal(19, resultE);
+
+            string text = File.ReadAllText("C:\\Dev\\Advent-Of-Code-2022\\AdventOfCode2022_Day05\\input_day06.txt");
+            int result = solution.Part02(text);
+
         }
     }
 }
